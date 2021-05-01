3426973748c6971e1c18ec90eeb8007811406de4
Denis Dimchev <denis.dimchev@gmail.com>
2021-05-01 22:20:29 +0200

    Added Client

---
+++ client/src/components/settimana/settimana.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './settimana.css'
+
+function settimana() {
+    return (
+        <div className="glass">
+            <h3>Temperatura:</h3>
+            <h3>Probabilita di pioggia:%</h3>
+        </div> 
+    )
+}
+
+export default settimana
